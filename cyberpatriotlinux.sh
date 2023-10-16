1890659e0cf6b5d8b5a9493930a8dfeba5c29600
Ryan Samuels <131621582+returnofblank@users.noreply.github.com>
2023-10-15 20:05:51 -0400

    wasd

--- cyberpatriotlinux.sh
+++ cyberpatriotlinux.sh
@@ -140,10 +140,10 @@ while true; do
             usermod -aG "$group_name" "$user" &>/dev/null
           done
 
-          sudodelorno=$(dialog --title "Remove sudo privileges from the following users?" --yesno "Are you sure you want to remove the following user's admin privileges?" 0 0 --output-fd 1)
+          sudodelorno=$(dialog --title "Remove sudo privileges from the following users?" --yesno "Are you sure you want to remove the following user's admin privileges?: ${users_to_remove[*]}" 0 0 --output-fd 1)
           if [ "$sudodelorno" = "1" ]; then
             dialog --msgbox "No users removed from sudo group. User selected no." 0 0
-            continue
+            users_to_remove=""
           else
           # Remove users from the sudo group
             for user in "${users_to_remove[@]}"; do
